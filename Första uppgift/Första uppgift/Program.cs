48130fe9dbcc4f6f54cf94cb849e00b1b70376de
Ivanovic14 <ival06001@utb.vaxjo.se>
2023-11-24 11:40:11 +0100

    uppg1

---
+++ Första uppgift/Första uppgift/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace förstauppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett ord som ska upprepas 5 gånger.");
+
+            string ord = Console.ReadLine();
+
+            for (int i = 0; i < 9; i++)
+            {
+                Console.Write(ord);
+            }
+        }
+    }
+}
